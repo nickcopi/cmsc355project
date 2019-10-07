64916ae2ff3516dc793c8f8abd5a73a3915ec76e
copinf22 <copinf@mymail.vcu.edu>
2019-10-07 12:25:33 -0400

    Added two new activities for ingame menu and upgrade menu.

---
+++ app/src/main/java/one/group/oneapp/InGame.java
@@ -0,0 +1,14 @@
+package one.group.oneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_game);
+    }
+}
---
+++ app/src/main/java/one/group/oneapp/UpgradeMenu.java
@@ -0,0 +1,14 @@
+package one.group.oneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpgradeMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upgrade_menu);
+    }
+}
