f1bd69c155efca6c496883c28ca0479ffc3b629b
copinf22 <copinf@mymail.vcu.edu>
2019-10-21 12:37:52 -0400

    Revert "Attempt to merge colr scheme."
    This reverts commit 674f56ad516f08695bde2f8a1f128d7a4f9b1824.

--- app/src/main/java/one/group/oneapp/Player.java
+++ app/src/main/java/one/group/oneapp/Player.java
@@ -1,6 +1,23 @@
 package one.group.oneapp;
 
-public class Player implements Collidable{
+//import android.view.GestureDetector;
+//import android.view.MotionEvent;
+//import android.widget.ImageView;
+
+public class Player implements Collidable //GestureDetector.OnGestureListener
+{
+//    //No clue what the GestureDetector line does
+//    private GestureDetector gestureDetector = new GestureDetector(this);
+//    ImageView testImage;
+//
+//    //Image Dimensions
+//    int imageX;
+//    int imageY;
+//
+//    //Screen Dimensions
+//    private int screenWidth;
+//    private int screenHeight;
+
     private int x,y,width,height;
     private double speed;
     private Directions direction;
@@ -58,4 +75,148 @@ public class Player implements Collidable{
     public void setDirection(Directions direction) {
         this.direction = direction;
     }
+
+//    //Methods needed by GestureDetector
+//    @Override
+//    public boolean onDown(MotionEvent e) {
+//        return false;
+//    }
+//
+//    @Override
+//    public void onShowPress(MotionEvent e) {
+//
+//    }
+//
+//    @Override
+//    public boolean onSingleTapUp(MotionEvent e) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+//        return false;
+//    }
+//
+//    @Override
+//    public void onLongPress(MotionEvent e) {
+//
+//    }
+//
+//    @Override
+//    public boolean onFling(MotionEvent pressDown, MotionEvent moveFinger, float xAxis, float yAxis)
+//    {
+//        float xDistance = moveFinger.getX() - pressDown.getX(); //Distance of swipe in X direction
+//        float yDistance = moveFinger.getY() - pressDown.getY(); //Distance of swipe in Y direction
+//
+//        if(Math.abs(xDistance) > Math.abs(yDistance)) //If swiped farther in X direction then Y
+//        {
+//            if(Math.abs(xDistance) > 50 && Math.abs(xAxis) > 50)//Threshold for how far user has to swipe and how fast
+//            {
+//                if(xDistance > 0) //Swiped right
+//                {
+//                    moveImage("Right");
+//                    return true;
+//                }
+//                else //Swiped left
+//                {
+//                    moveImage("Left");
+//                    return true;
+//                }
+//            }
+//        }
+//        else
+//        {
+//            if(Math.abs(yDistance) > 50 && Math.abs(yAxis) > 50)
+//            {
+//                if(yDistance < 0) //Swiped up, Up is considered the negative direction????
+//                {
+//                    moveImage("Up");
+//                    return true;
+//                }
+//                else //Swiped down
+//                {
+//                    moveImage("Down");
+//                    return true;
+//                }
+//            }
+//        }
+//
+//        return false;//Only reached if user didnt swipe correctly
+//    }
+//
+//    //Moves image 100 pixels in swiped Direction
+//    private void moveImage(String Direction)
+//    {
+//        switch(Direction)
+//        {
+//            case "Up":
+//            {
+//                imageY = imageY - 100;
+//
+//                if(imageY < 0)
+//                {
+//                    imageY = 0;
+//                    testImage.setY(imageY);
+//                }
+//                else
+//                {
+//                    testImage.setY(imageY);
+//                }
+//
+//                break;
+//            }
+//            case "Down":
+//            {
+//                imageY = imageY + 100;
+//
+//                if(imageY > screenHeight)//If move will put past ceiling, just move picture to wall
+//                {
+//                    imageY = screenHeight;
+//                    testImage.setY(imageY);
+//                }
+//                else
+//                {
+//                    testImage.setY(imageY);
+//                }
+//
+//                break;
+//            }
+//            case "Left":
+//            {
+//                imageX = imageX - 100;
+//
+//                if(imageX < 0)
+//                {
+//                    imageX = 0;
+//                    testImage.setX(imageX);
+//                }
+//                else
+//                {
+//                    testImage.setX(imageX);
+//                }
+//
+//                break;
+//            }
+//            case "Right":
+//            {
+//                imageX = imageX + 100;
+//
+//                if(imageX > screenWidth)
+//                {
+//                    imageX = screenWidth;
+//                    testImage.setX(imageX);
+//                }
+//                else
+//                {
+//                    testImage.setX(imageX);
+//                }
+//
+//                break;
+//            }
+//        }
+//
+//        System.out.println("Direction moved: " + Direction + " x coordinates: " + imageX + " y coordinates: " + imageY);
+//    }
+
+
 }
