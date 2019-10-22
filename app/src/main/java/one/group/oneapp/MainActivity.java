a7ed43ccb78bea0d7db08dcee4b880e40ba0d67c
Troy L <lampheretroy99@gmail.com>
2019-10-22 14:41:07 -0400

    added text and image to main menu

--- app/src/main/java/one/group/oneapp/MainActivity.java
+++ app/src/main/java/one/group/oneapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
@@ -16,7 +17,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.lawnmower);
+
     }
     public void clickPlay(View view){
         Intent intent = new Intent(MainActivity.this,InGame.class);
