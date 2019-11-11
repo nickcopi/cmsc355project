4979dd36dff65a71f94c3fa0cfe465294ee17d7a
Troy L <lampheretroy99@gmail.com>
2019-11-11 14:15:49 -0500

    added stopwatch

---
+++ app/src/main/java/one/group/oneapp/Stopwatch.java
@@ -0,0 +1,23 @@
+package one.group.oneapp;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.Chronometer;
+
+import static android.view.View.VISIBLE;
+
+public class Stopwatch extends Activity {
+
+    private Chronometer timeView;
+
+    public Stopwatch() {
+
+    }
+
+    public void makeVisible(){
+        //timeView = findViewById(R.id.timeView);
+
+        //timeView.setVisibility(VISIBLE);
+    }
+
+}
--- app/src/main/java/one/group/oneapp/TimerUpgrade.java
+++ app/src/main/java/one/group/oneapp/TimerUpgrade.java
@@ -2,4 +2,40 @@ package one.group.oneapp;
 
 public class TimerUpgrade extends AbstractUpgrade
 {
+    private int cost;
+    private int level;
+    private String upgradeName;
+    private String upgradeDescription;
+    private boolean locked;
+
+
+    TimerUpgrade(int costAmount, String nameOfUpgrade, String descriptionOfUpgrade) {
+        cost = costAmount;
+        level = 1;
+        upgradeName = nameOfUpgrade;
+        upgradeDescription = descriptionOfUpgrade;
+        locked = true;
+    }
+
+    public String buy(Wallet playerWallet){
+        if (locked = false)
+        {
+            int price = getCost();
+
+            if (playerWallet.getMoney() > price)
+            {
+                playerWallet.setMoney(playerWallet.getMoney() - price);
+                ++level;
+
+                return "Upgrade Purchased";
+            }
+            else
+            {
+                return "Broke boy";
+            }
+
+        }
+
+        return upgradeName + " Upgrade is locked";
+    
 }
